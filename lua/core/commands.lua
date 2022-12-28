b8430f083f6c519895d1939861d8ab4b43fc2658
Rey Nazareno <r.nazbat@gmail.com>
2022-12-29 01:44:48 +0800

    comment out last buffer command

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,21 +1,21 @@
--- Close nvim-tree when it's the last buffer
-local modifiedBufs = function(bufs)
-  local t = 0
-  for k, v in pairs(bufs) do
-    if v.name:match("NvimTree_") == nil then
-      t = t + 1
-    end
-  end
-  return t
-end
+-- -- Close nvim-tree when it's the last buffer
+-- local modifiedBufs = function(bufs)
+--   local t = 0
+--   for k, v in pairs(bufs) do
+--     if v.name:match("NvimTree_") == nil then
+--       t = t + 1
+--     end
+--   end
+--   return t
+-- end
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and
-        vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
-        modifiedBufs(vim.fn.getbufinfo({ bufmodified = 1 })) == 0 then
-      vim.cmd "quit"
-    end
-  end
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   nested = true,
+--   callback = function()
+--     if #vim.api.nvim_list_wins() == 1 and
+--         vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
+--         modifiedBufs(vim.fn.getbufinfo({ bufmodified = 1 })) == 0 then
+--       vim.cmd "quit"
+--     end
+--   end
+-- })
