329a79276268dd1721d1a7ef05098111ec996351
Rey Nazareno <r.nazbat@gmail.com>
2023-01-01 15:41:41 +0800

    add old lualine config for reference

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -63,3 +63,58 @@ require('lualine').setup({
     end }, { 'location' } }
   }
 })
+
+
+-- require('lualine').setup {
+--   options = {
+--     icons_enabled = true,
+--     theme = 'auto',
+--     component_separators = { left = '', right = ''},
+--     section_separators = { left = '', right = ''},
+--     disabled_filetypes = {
+--       statusline = {},
+--       winbar = {},
+--     },
+--     ignore_focus = {},
+--     always_divide_middle = true,
+--     globalstatus = false,
+--     refresh = {
+--       statusline = 1000,
+--       tabline = 1000,
+--       winbar = 1000,
+--     }
+--   },
+--   sections = {
+--     lualine_a = {'mode'},
+--     lualine_b = {'branch', 'diff', 'diagnostics'},
+--     lualine_c = {'filename'},
+--     lualine_x = {'filetype'},
+--     lualine_y = {'progress'},
+--     lualine_z = {'location'}
+--   },
+--   inactive_sections = {
+--     lualine_a = {},
+--     lualine_b = {},
+--     lualine_c = {'filename'},
+--     lualine_x = {'location'},
+--     lualine_y = {},
+--     lualine_z = {}
+--   },
+--   tabline = {},
+--   winbar = {},
+--   inactive_winbar = {},
+--   extensions = {}
+-- }
+
+-- require('onedark').setup {
+--   colors = {
+--     bright_orange = "#ff8800",    -- define a new color
+--     green = '#00ffaa',            -- redefine an existing color
+--   },
+--   highlights = {
+--     ["@keyword"] = {fg = '$green'},
+--     ["@string"] = {fg = '$bright_orange', bg = '#00ff00', fmt = 'bold'},
+--     ["@function"] = {fg = '#0000ff', sp = '$cyan', fmt = 'underline,italic'},
+--     ["@function.builtin"] = {fg = '#0059ff'}
+--   }
+-- }
