e4d1122f66c09e57d97e7fc203cd0e1879b57d8d
Rey Nazareno <r.nazbat@gmail.com>
2023-01-03 09:05:09 +0800

    update telescope bindings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,9 +8,10 @@ end
 map_key('n', '<Leader>h', ':noh <CR>')
 
 -- Telescope
-map_key('n', '<Leader>t', ':Telescope<CR>')
+map_key('n', ',t', ':Telescope<CR>')
 map_key('n', ',f', ':Telescope find_files<CR>') -- dunno why yet but <Leader>f or <Leader><any> has a delay on this command
-map_key('n', '<Leader>f', ':Telescope find_files hidden=true<CR>')
+map_key('n', ',fh', ':Telescope find_files hidden=true<CR>')
+map_key('n', ',fg', ':Telescope live_grep<CR>')
 
 -- Nvim-tree
 map_key('n', '<Leader>b', ':NvimTreeToggle <CR>')
